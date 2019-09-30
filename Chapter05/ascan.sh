883d70fe225b4fb4c5c07f131762015051b15fb6
diassor <gruasedwin@gmail.com>
2019-09-30 11:13:22 -0400

    sync

--- Chapter05/ascan.sh
+++ Chapter05/ascan.sh
@@ -5,3 +5,8 @@ arachni $1 \
     --scope-include-subdomains \
     --timeout 0:05:00 \
     --http-request-concurrency 10
+
+#    Like bootstrap_burp.sh we  can make it executables through a
+#    simple chmod u+x ascan.sh and add it into pou path by
+#    using
+#        sudo ln -s /path/to/ascan.sh  /usr/local/bin/ascan 
